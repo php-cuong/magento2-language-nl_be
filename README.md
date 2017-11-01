c56b1aa6f2571c65a89b3491be9c90025d945dd4
php-cuong <bestearnmoney87@gmail.com>
2017-11-02 01:55:05 +0700

    init source code

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # magento2-language-nl_be
-magento2-language-nl_be
+Magento 2 the Dutch, Belgium language package, Dutch, Belgium translation
---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @Author: Ngo Quang Cuong
+ * @Date:   2017-07-11 03:54:20
+ * @Last Modified by:   https://www.facebook.com/giaphugroupcom
+ * @Last Modified time: 2017-11-02 01:51:31
+ * @website: http://giaphugroup.com
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'phpcuong_nl_be',
+    __DIR__
+);
